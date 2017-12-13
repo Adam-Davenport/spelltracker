6b3738bac5d2c68f724587fe460ecbf47116082e
Adam-Davenport <adavenpo@purdue.edu>
2017-12-13 10:58:12 -0500

    Creating form to create character

---
+++ spelltracker/forms.py
@@ -0,0 +1,12 @@
+from django import forms
+from spelltracker.models import Character
+
+
+class Character_Form(forms.ModelForm):
+
+    class Meta:
+        model = Character
+        fields = ('name')
+        widgets = {
+            'name': forms.TextInput(attrs={'class': 'form-control'})
+        }

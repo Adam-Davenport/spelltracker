3ac6474bf88c19c9f6341492ce3148dd78cdad63
Adam-Davenport <adavenpo@purdue.edu>
2017-12-15 14:43:56 -0500

    Fixing errors

--- setup.sh
+++ setup.sh
@@ -9,14 +9,14 @@ source ~/.bashrc
 sudo apt update
 
 # Web server setup
-sudo apt install nginx gunicorn upstart -y
+sudo apt install nginx gunicorn supervisor -y
 sudo rm /etc/nginx/sites-enabled/default
 sudo cp .setup/spelltracker /etc/nginx/sites-enabled/spelltracker
 sudo service nginx restart
 
 # Python setup
-sudo apt install python3
-sudo apt install python-pip
+sudo apt install python3 -y
+sudo apt install python-pip -y
 pip install Django==1.11
 
 # Galacticweapons setup

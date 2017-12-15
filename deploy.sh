f85c1bb990910136ca5037049722b1acd152a822
Adam-Davenport <adavenpo@purdue.edu>
2017-12-15 14:07:07 -0500

    Adding all deployment scripts

---
+++ deploy.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+python manage.py migrate
+python manage.py makemigrations
+python manage.py migrate
+python manage.py collectstatic
+gunicorn spelltracker.wsgi
---
+++ setup.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+# This script is used to setup on a new server
+
+# Copy bashrc file
+cp .setup/.bashrc ~/.bashrc
+source ~/.bashrc
+
+# Update repository information
+sudo apt update
+
+# Web server setup
+sudo apt install nginx gunicorn upstart -y
+sudo rm /etc/nginx/sites-enabled/default
+sudo cp .setup/spelltracker /etc/nginx/sites-enabled/spelltracker
+sudo service nginx restart
+
+# Python setup
+sudo apt install python3
+sudo apt install python-pip
+
+# Galacticweapons setup
+python manage.py migrate
+python manage.py makemigrations
+python manage.py migrate
+python manage.py collectstatic
+
+# Supervisor setup
+sudo cp .setup/gunicorn.conf /etc/init/gunicorn.conf
+sudo service supervisor restart
